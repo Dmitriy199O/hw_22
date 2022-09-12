7472a747dddf6bf1af79c13303fd6705a6120573
dmitry <mityai20031990@gmail.com>
2022-09-12 22:21:32 +0300

    Initial commit

---
+++ part1/practice/bad_smell_long_parameter_list/main.py
@@ -0,0 +1,39 @@
+# У нас есть какой-то юнит, которому мы в параметры передаем
+# - наше игровое поле
+# - х координату
+# - у координату
+# - направление смещения
+# - летит ли он
+# - крадется ли он
+# - скорость
+# В этом примере есть сразу несколько запахов плохого кода. Исправьте их
+#   (длинный метод, длинный список параметров)
+
+
+class Unit:
+    def __init__(self, state, speed):
+        self.state = state
+        self.speed = speed
+
+    def _get_speed(self):
+        if self.state == 'fly':
+            self.speed = 1.2
+            return self.speed
+
+        elif self.state == 'crawl':
+            self.speed = 0.5
+
+        else:
+            raise ValueError('Что ты за зверь такой... Хм?')
+
+    def move(self, field, x_coord, y_coord, direction):
+
+
+        if direction == 'UP':
+            self.field.set_unit(y=self.y + self.speed, x=self.x, unit=self)
+        elif direction == 'DOWN':
+            self.field.set_unit(y=self.y - self.speed, x=self.x, unit=self)
+        elif direction == 'LEFT':
+            self.field.set_unit(y=self.y, x=self.x - self.speed, unit=self)
+        elif direction == 'RIGTH':
+            self.field.set_unit(y=self.y, x=self.x + self.speed, unit=self)
